560a9d31fb6ce5355db4c74e63d5ff049a18098a
teg-hub <basette@theempowergroup.org>
2025-11-06 21:10:41 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,15 +7,15 @@ import { Database } from './types'
 export function supabaseServer() {
   const c = cookies()
   return createServerClient<Database>(
-    process.env.https://gktfwtcngzxxoazhoshs.supabase.co!,
-    process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrdGZ3dGNuZ3p4eG9hemhvc2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NTk4ODksImV4cCI6MjA3ODAzNTg4OX0.OHBfU-wRhnqJ4JKIw7JWRQMmq83t35iPbppNICpio7w!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     { cookies: { get: (name) => c.get(name)?.value } }
   )
 }
 
 export function supabaseBrowser() {
   return createClient<Database>(
-    process.env.https://gktfwtcngzxxoazhoshs.supabase.co!,
-    process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrdGZ3dGNuZ3p4eG9hemhvc2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NTk4ODksImV4cCI6MjA3ODAzNTg4OX0.OHBfU-wRhnqJ4JKIw7JWRQMmq83t35iPbppNICpio7w!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
