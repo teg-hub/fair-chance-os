7e9f9a92d9dc94803351291292a5a8e9d3d113fd
teg-hub <basette@theempowergroup.org>
2025-11-06 19:49:23 -0500

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,21 @@
+'use server'
+import { cookies } from 'next/headers'
+import { createServerClient } from '@supabase/ssr'
+import { createClient } from '@supabase/supabase-js'
+import { Database } from './types'
+
+export function supabaseServer() {
+  const c = cookies()
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies: { get: (name) => c.get(name)?.value } }
+  )
+}
+
+export function supabaseBrowser() {
+  return createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
