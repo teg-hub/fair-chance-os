7e2495f0984fe5b2bc9bda631fd54e1e2e0d900b
teg-hub <basette@theempowergroup.org>
2025-11-06 21:07:27 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { serverActions: { bodySizeLimit: '10mb' } }
-}
-module.exports = nextConfig
+  experimental: { serverActions: { bodySizeLimit: '10mb' } },
+};
+module.exports = nextConfig;
