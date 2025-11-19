637c4bb24c4f7faeacd2381033585291a2976c63
teg-hub <basette@theempowergroup.org>
2025-11-19 16:24:58 -0500

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from 'tailwindcss'
-const config: Config = {
-  content: ["./src/**/*.{ts,tsx}"],
-  theme: { extend: { colors: { brand: { 50:'#eef6ff', 700:'#1d4ed8' } } } },
-  plugins: []
-}
-export default config
