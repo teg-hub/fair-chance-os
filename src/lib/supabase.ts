bf11253fced1f99227503d23f75f73d9ebdde828
teg-hub <basette@theempowergroup.org>
2025-11-19 17:39:56 -0500

    Delete src/lib/supabase.ts

--- src/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createBrowserClient, createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
-export const createSbBrowser = () => createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
-export const createSbServer = () => {
-  const store = cookies()
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-    cookies: { get: (name: string) => store.get(name)?.value }
-  })
-}
